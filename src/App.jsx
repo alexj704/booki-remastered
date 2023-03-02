4c1822e081b4e8a6cac3b03639308ca1fa535201
Kroumy <alexandre.joignant@gmail.com>
2023-03-02 19:36:31 +0100

    Ajout page d'erreur

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,15 @@
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+  Navigate,
+} from "react-router-dom";
 import Header from "./components/Header/Header";
 import Footer from "./components/Footer/Footer";
 import Home from "./pages/Home/Home";
 import Hébergements from "./pages/Hébergements/Hébergements";
 import Hébergement from "./pages/Hébergement/Hébergement";
+import Error from "./components/Error/Error";
 import "./App.css";
 
 function App() {
@@ -14,6 +20,8 @@ function App() {
         <Route path="/" element={<Home />} />
         <Route path="/hebergements/all" element={<Hébergements />} />
         <Route path="/hebergements/:id" element={<Hébergement />} />
+        <Route path="/notfound" element={<Error />} />
+        <Route path="*" element={<Navigate to="/notfound" />} />
       </Routes>
       <Footer />
     </Router>
